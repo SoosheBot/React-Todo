aa3edc6e68e42453e13ed3f10c2cb6905eaec7aa
sooshe <sooshe@sooshebot.com>
2020-01-13 15:12:02 -0600

    creates addtodo function in app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 
+import TodoList from './components/TodoComponents/TodoForm';
+import TodoList from './components/TodoComponents/TodoList';
+
 class App extends Component {
   // you will need a place to store your state in this component.
   constructor() {
@@ -10,6 +13,17 @@ class App extends Component {
   };
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+
+  addToDo = todoText => {
+    const newTodo = {
+      todo: todoText,
+      taskCompleted:false,
+      id:Date.now()
+    };
+
+    this.setState({todoList: [...this.state.todoList, newTodo]})
+  }
+
   render() {
     return (
       <div>
